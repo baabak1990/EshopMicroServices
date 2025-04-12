e62e314536e12c37b53d6bbe65fdab227d44181d
baabak aghaei <baabakaghaei@gmail.com>
2025-04-12 20:10:36 +0330

    No thing Important

--- src/Services/Catalog/Catalog.API/Model/Product.cs
+++ src/Services/Catalog/Catalog.API/Model/Product.cs
@@ -7,6 +7,6 @@
         public string Description { get; set; } = default!;
         public List<string> Category { get; set; } = new();
         public string ImageFile { get; set; }= default!;
-        public decimal Price { get; set; }
+        public decimal Price { get; set; } 
     }
 }
