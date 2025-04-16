ab1e3b7fdaeb21952ebc856d9eb1595d3c3b3307
baabak aghaei <baabakaghaei@gmail.com>
2025-04-16 21:36:33 +0330

    adding mapster and Carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,5 +1,17 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlock.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
 {
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+
+     
     public class CreateProductEndPoint
     {
     }
