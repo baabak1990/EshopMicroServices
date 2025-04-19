257434bb86cf076e5c56a03d95b1f631bc60e28f
baabak aghaei <baabakaghaei@gmail.com>
2025-04-19 20:30:25 +0330

    not much

---
+++ MultiThreadingExamples/MultiThreadingExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MultiThreadingExamples
+{
+    internal class Program
+    {
+
+        static async Task<decimal> calcualte(decimal a, decimal b)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(10));
+            Console.WriteLine($"Sum is running in a thread{Environment.CurrentManagedThreadId}");
+
+            return a + b;
+        }
+        static async Task Main(string[] args)
+        {
+            await calcualte(5, 6);
+
+        }
+    }
+}
---
+++ MultiThreadingExamples/StateMachineDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace StateMachineDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static async void FoodAsync()
+        {
+
+        }
+
+        static void Food()
+        {
+            
+        }
+
+        struct FoodAsyncStateMachine : IAsyncStateMachine
+        { 
+            public void MoveNext()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+
+
+}
